ee0e0089930b970902ac3c0f37fe219a5604e099
Petr <petr@duck.com>
2024-08-11 20:41:01 +0300

    trying with podman driver

--- my/rebuild-and-launch.sh
+++ my/rebuild-and-launch.sh
@@ -3,10 +3,13 @@
 # delete if exists
 minikube delete -p petr-polyakov-kuber-sf
 
+# needed for podman
+minikube config set rootless true
+
 # create
-minikube config set driver docker
+minikube config set driver podman
 minikube kubectl -- get po -A
-minikube start --nodes 3 -p petr-polyakov-kuber-sf --driver=docker --mount-string="$(pwd)/src:/var/www/html" --mount-string="$(pwd)/db-init:/data/application"
+minikube start --nodes 3 -p petr-polyakov-kuber-sf --driver=podman --mount-string="$(pwd)/src:/var/www/html" --mount-string="$(pwd)/db-init:/data/application"
 
 # setting default and alias
 minikube config set profile petr-polyakov-kuber-sf
