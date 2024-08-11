a030af096f4a43682fdf4af85f3b3b78c0eaffa6
Petr <petr@duck.com>
2024-08-11 17:21:15 +0300

    switched to mounting on launch + removed alias (it will not work anyway)

--- my/rebuild-and-launch.sh
+++ my/rebuild-and-launch.sh
@@ -6,15 +6,15 @@ minikube delete -p petr-polyakov-kuber-sf
 # create
 minikube config set driver docker
 minikube kubectl -- get po -A
-minikube start --nodes 3 -p petr-polyakov-kuber-sf --driver=docker # --mount-string="$(pwd)/src:/var/www/html" --mount-string="$(pwd)/db-init:/data/application"
+minikube start --nodes 3 -p petr-polyakov-kuber-sf --driver=docker --mount-string="$(pwd)/src:/var/www/html" --mount-string="$(pwd)/db-init:/data/application"
 
 # setting default and alias
 minikube config set profile petr-polyakov-kuber-sf
-alias kubectl="minikube kubectl --"
+# alias kubectl="minikube kubectl --"
 
 # mounts
-minikube mount $(pwd)/src:/var/www/html &!
-minikube mount $(pwd)/db-init:/data/application &!
+# minikube mount $(pwd)/src:/var/www/html &!
+# minikube mount $(pwd)/db-init:/data/application &!
 
 # adding objects from config
-kubectl apply -f configmap.yaml,secret.yaml,web.yaml,db.yaml
+minikube kubectl -- apply -f configmap.yaml,secret.yaml,web.yaml,db.yaml
